b4afd4976bdabe14d2fb08b31c5cd3a79cfcf336
Cheruiyot Collins <kelvincollins86@gmail.com>
2023-06-26 19:07:03 +0300

    First Commit

---
+++ src/main/java/com/ezra/elevator/ElevetorApplication.java
@@ -0,0 +1,13 @@
+package com.ezra.elevator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElevetorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElevetorApplication.class, args);
+	}
+
+}
